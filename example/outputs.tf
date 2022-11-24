25b0dedefea8e779ac975c62115d0c3a501fec31
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-11-24 16:59:28 +0000

    fix output

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,8 +1,3 @@
-output "private_dns_zone_name" {
-  description = "The name of the private dns zone"
-  value       = module.azure_private_dns_terratest.private_dns_zone
-}
-
 output "a_record" {
   description = "The A records of the private dns zone"
   value       = module.azure_private_dns_terratest.a_record
