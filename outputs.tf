8b65f51fa1e7693c551eeb4222aa075bc5b8a710
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-11-24 17:56:21 +0000

    fix output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "cname_record" {
   description = "The name of the Resource Group"
   value       = azurerm_private_dns_cname_record.this
 }
+
+output "private_dns_zone" {
+  description = "The name of the Resource Group"
+  value       = var.create_zone ? azurerm_private_dns_zone.zone.0.name : ""
+}
