081988ca7c83b8a083143d63d84eb6355583fe74
varunvijayagopal <105358638+varunvijayagopal@users.noreply.github.com>
2022-11-24 16:40:17 +0000

    Update outputs.tf
    remove dns zone from recode as this will not be present all the time

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "cname_record" {
   description = "The name of the Resource Group"
   value       = azurerm_private_dns_cname_record.this
 }
-
-output "private_dns_zone" {
-  description = "The name of the Resource Group"
-  value       = azurerm_private_dns_zone.zone.0.name
-}
